86f2e2623ea0b365a6a2051b1c10ef77d43600fb
Filipstrozik <filipshelby@gmail.com>
2022-12-02 10:38:04 +0100

    all exercises

--- lista03Developer.sql
+++ lista03Developer.sql
@@ -35,8 +35,8 @@ BEGIN
     END IF;
 
     EXCEPTION
-    WHEN NO_DATA_FOUND THEN DBMS_OUTPUT.PUT_LINE('BRAK TAKIEGO KOTA');
-    WHEN OTHERS THEN DBMS_OUTPUT.PUT_LINE(sqlerrm);
+        WHEN NO_DATA_FOUND THEN DBMS_OUTPUT.PUT_LINE('BRAK TAKIEGO KOTA');
+        WHEN OTHERS THEN DBMS_OUTPUT.PUT_LINE(sqlerrm);
 END;
 
 --zad36
@@ -198,60 +198,108 @@ EXCEPTION
 END;
 
 --zad40
-CREATE OR REPLACE PROCEDURE AddBanda(nr_ban BANDY.NR_BANDY%TYPE,
+CREATE OR REPLACE PACKAGE PACK IS
+    FUNCTION ObliczPodatek(pseudonim KOCURY.PSEUDO%TYPE) RETURN NUMBER;
+    PROCEDURE  AddBanda(nr_ban BANDY.NR_BANDY%TYPE,
+                                    naz_ban BANDY.NAZWA%TYPE,
+                                    ter BANDY.TEREN%TYPE);
+END PACK;
+
+CREATE OR REPLACE PACKAGE BODY PACK IS
+    FUNCTION ObliczPodatek(pseudonim KOCURY.PSEUDO%TYPE) RETURN NUMBER
+        IS
+        wysokosc_podatku NUMBER := 0;
+        ile NUMBER := 0;
+        BEGIN
+            SELECT CEIL(0.05 * (NVL(przydzial_myszy,0) + NVL(myszy_extra,0)))
+            INTO wysokosc_podatku
+            FROM KOCURY
+            WHERE pseudo = pseudonim;
+            
+            SELECT COUNT(pseudo) 
+            INTO ile 
+            FROM KOCURY
+            WHERE szef = pseudonim;
+            
+            IF ile = 0 THEN
+                wysokosc_podatku := wysokosc_podatku + 2;
+            END IF;
+            
+            SELECT COUNT(pseudo) INTO ile FROM WROGOWIE_KOCUROW WHERE pseudo = pseudonim;
+            if ile = 0 THEN
+                wysokosc_podatku := wysokosc_podatku + 1;
+            END IF;
+            SELECT NVL(przydzial_myszy,0) INTO ile FROM KOCURY WHERE pseudo = pseudonim;
+            IF ile > 20 THEN
+                wysokosc_podatku := wysokosc_podatku + 5;
+            END IF;
+        RETURN wysokosc_podatku;
+        END;
+
+    PROCEDURE AddBanda(nr_ban BANDY.NR_BANDY%TYPE,
                                     naz_ban BANDY.NAZWA%TYPE,
                                     ter BANDY.TEREN%TYPE)
-IS
-    liczba_znalezionych NUMBER;
-    juz_istnieje_exc EXCEPTION;
-    zly_numer_bandy_exc EXCEPTION;
-    wiadomosc_exc    VARCHAR2(30) := '';
-BEGIN
-    IF nr_ban < 0 THEN RAISE zly_numer_bandy_exc;
-    END IF;
-    
-    SELECT COUNT(*) INTO liczba_znalezionych
-    FROM Bandy
-    WHERE nr_bandy = nr_ban;
-    IF liczba_znalezionych <> 0 
-        THEN wiadomosc_exc := wiadomosc_exc || ' ' || nr_ban || ',';
-    END IF;
-    
-    SELECT COUNT(*) INTO liczba_znalezionych
-    FROM Bandy
-    WHERE nazwa = naz_ban;
-    IF liczba_znalezionych <> 0 
-        THEN wiadomosc_exc := wiadomosc_exc || ' ' || naz_ban || ',';
-    END IF;
-    
-    SELECT COUNT(*) INTO liczba_znalezionych
-    FROM Bandy
-    WHERE teren = ter;
-    IF liczba_znalezionych <> 0 
-        THEN wiadomosc_exc := wiadomosc_exc || ' ' || ter || ',';
-    END IF;
-    
-    IF LENGTH(wiadomosc_exc) > 0 THEN
-        RAISE juz_istnieje_exc;
-    END IF;
-    
-    INSERT INTO BANDY(NR_BANDY, NAZWA, TEREN) VALUES (nr_ban, naz_ban, ter);
-EXCEPTION
-    WHEN zly_numer_bandy_exc THEN
-        DBMS_OUTPUT.PUT_LINE('Nr bandy musi byc liczba dodatnia');
-    WHEN juz_istnieje_exc THEN
-        DBMS_OUTPUT.PUT_LINE(TRIM(TRAILING ',' FROM wiadomosc_exc) || ': juz istnieje');
-END;
+        IS
+            liczba_znalezionych NUMBER;
+            juz_istnieje_exc EXCEPTION;
+            zly_numer_bandy_exc EXCEPTION;
+            wiadomosc_exc    VARCHAR2(30) := '';
+        BEGIN
+            IF nr_ban < 0 THEN 
+                RAISE zly_numer_bandy_exc;
+            END IF;
+        
+            SELECT COUNT(*) 
+            INTO liczba_znalezionych
+            FROM Bandy
+            WHERE nr_bandy = nr_ban;
+            
+            IF liczba_znalezionych <> 0 
+                THEN wiadomosc_exc := wiadomosc_exc || ' ' || nr_ban || ',';
+            END IF;
+        
+            SELECT COUNT(*) INTO liczba_znalezionych
+            FROM Bandy
+            WHERE nazwa = naz_ban;
+            IF liczba_znalezionych <> 0 
+                THEN wiadomosc_exc := wiadomosc_exc || ' ' || naz_ban || ',';
+            END IF;
+            
+            SELECT COUNT(*) INTO liczba_znalezionych
+            FROM Bandy
+            WHERE teren = ter;
+            IF liczba_znalezionych <> 0 
+                THEN wiadomosc_exc := wiadomosc_exc || ' ' || ter || ',';
+            END IF;
+            
+            IF LENGTH(wiadomosc_exc) > 0 THEN
+                RAISE juz_istnieje_exc;
+            END IF;
+        
+            INSERT INTO BANDY(NR_BANDY, NAZWA, TEREN) VALUES (nr_ban, naz_ban, ter);
+        EXCEPTION
+            WHEN zly_numer_bandy_exc THEN
+                DBMS_OUTPUT.PUT_LINE('Nr bandy musi byc liczba dodatnia');
+            WHEN juz_istnieje_exc THEN
+                DBMS_OUTPUT.PUT_LINE(TRIM(TRAILING ',' FROM wiadomosc_exc) || ': juz istnieje');
+    END;
+END PACK;
 
-EXECUTE AddBanda(1, 'PUSZYSCI', 'POLE');
-EXECUTE AddBanda(2, 'CZARNI RYCERZE', 'POLE');
-EXECUTE AddBanda(1, 'SZEFOSTWO', 'NOWE');
-EXECUTE AddBanda(10, 'NOWI', 'NOWE');
+EXECUTE PACK.AddBanda(1, 'PUSZYSCI', 'POLE');
+EXECUTE PACK.AddBanda(2, 'CZARNI RYCERZE', 'POLE');
+EXECUTE PACK.AddBanda(1, 'SZEFOSTWO', 'NOWE');
+EXECUTE PACK.AddBanda(10, 'NOWI', 'NOWE');
 SELECT * FROM bandy;
 
 ROLLBACK;
+BEGIN
+        FOR kot IN (SELECT pseudo FROM Kocury)
+        LOOP
+            DBMS_OUTPUT.PUT_LINE(RPAD(kot.pseudo, 8) || ' podatek równy ' || PACK.ObliczPodatek(kot.pseudo));
+        END LOOP;
+END;
 
-
+--zad41
 CREATE OR REPLACE TRIGGER BiggerThenLastNumber
     BEFORE INSERT 
     ON BANDY
@@ -521,3 +569,123 @@ BEGIN
     DBMS_OUTPUT.put(LPAD(ilosc, 10));
     DBMS_OUTPUT.new_line();
 END;
+
+
+--zad45
+CREATE TABLE Dodatki_extra(
+    pseudo VARCHAR2(15) CONSTRAINT dodatki_pseudo_fk REFERENCES Kocury(pseudo),
+    dod_extra NUMBER(3) DEFAULT 0    
+);
+
+SELECT * FROM Dodatki_extra;
+
+DROP TABLE Dodatki_extra;
+
+
+--45 sprawdz to
+CREATE OR REPLACE TRIGGER trg_tygrys_kara
+    BEFORE UPDATE OF PRZYDZIAL_MYSZY
+    ON KOCURY
+    FOR EACH ROW
+DECLARE
+    PRAGMA AUTONOMOUS_TRANSACTION;
+BEGIN
+    IF LOGIN_USER <> 'TYGRYS' AND :NEW.PRZYDZIAL_MYSZY > :OLD.PRZYDZIAL_MYSZY AND :NEW.FUNKCJA = 'MILUSIA' THEN
+        EXECUTE IMMEDIATE
+            'DECLARE
+                ILE NUMBER;
+                DOD NUMBER;
+                CURSOR milusie IS SELECT PSEUDO
+                FROM KOCURY
+                WHERE funkcja = ''MILUSIA'';
+            BEGIN
+            FOR milusia IN milusie
+                LOOP
+                SELECT COUNT(*) INTO ILE FROM DODATKI_EXTRA WHERE PSEUDO = milusia.PSEUDO;
+                IF ILE = 0 THEN
+                    INSERT INTO DODATKI_EXTRA(PSEUDO, DOD_EXTRA) VALUES(milusia.PSEUDO, -10);
+                    ELSE
+                        SELECT DOD_EXTRA INTO DOD FROM DODATKI_EXTRA WHERE PSEUDO = milusia.PSEUDO;
+                        UPDATE DODATKI_EXTRA SET DOD_EXTRA = DOD - 10 WHERE PSEUDO = milusia.PSEUDO;
+                    END IF;
+                END LOOP;
+            END;';
+
+        COMMIT;
+    END IF;
+END;
+
+UPDATE KOCURY
+SET PRZYDZIAL_MYSZY = 100
+WHERE IMIE = 'SONIA';
+
+UPDATE KOCURY
+SET przydzial_myszy = 200
+WHERE imie = 'SONIA';
+
+SELECT *
+FROM KOCURY
+WHERE FUNKCJA = 'MILUSIA';
+
+SELECT * FROM Dodatki_extra;
+
+ROLLBACK;
+
+DROP TRIGGER trg_tygrys_kara;
+
+
+--zad 46
+--tabela
+CREATE TABLE Proby_wykroczenia 
+(
+    kto VARCHAR2(15) NOT NULL, --klucz obcy?
+    kiedy DATE NOT NULL,
+    jakiemu VARCHAR2(15) NOT NULL,
+    operacja VARCHAR2(15) NOT NULL
+);
+
+DROP TABLE Proby_wykroczenia;
+
+--wyzwalacz
+CREATE OR REPLACE TRIGGER trg_monitor_wykroczenia
+    BEFORE INSERT OR UPDATE OF PRZYDZIAL_MYSZY
+    ON KOCURY
+    FOR EACH ROW
+DECLARE
+    min_mysz FUNKCJE.MIN_MYSZY%TYPE;
+    max_mysz FUNKCJE.MAX_MYSZY%TYPE;
+    poza EXCEPTION;
+    curr_data DATE DEFAULT SYSDATE;
+    zdarzenie VARCHAR2(20);
+    PRAGMA AUTONOMOUS_TRANSACTION;
+BEGIN
+    SELECT MIN_MYSZY, MAX_MYSZY INTO min_mysz, max_mysz FROM FUNKCJE WHERE FUNKCJA = :NEW.FUNKCJA;
+    IF max_mysz < :NEW.PRZYDZIAL_MYSZY OR min_mysz > :NEW.PRZYDZIAL_MYSZY THEN
+        IF INSERTING THEN 
+            zdarzenie := 'INSERT';
+        ELSIF UPDATING THEN
+            zdarzenie := 'UPDATE';
+        END IF;
+        INSERT INTO Proby_wykroczenia(kto, kiedy, jakiemu, operacja) VALUES (ORA_LOGIN_USER, curr_data, :NEW.PSEUDO, zdarzenie);
+        COMMIT;
+        RAISE_APPLICATION_ERROR(-20001,'przydzial myszy poza zakresem funkcji, nie wykonano zmian.');
+        --:NEW.PRZYDZIAL_MYSZY := :OLD.PRZYDZIAL_MYSZY;
+        --RAISE poza;
+    END IF;
+--EXCEPTION
+    --WHEN poza THEN
+        --DBMS_OUTPUT.PUT_LINE('poza zakresem');
+END;
+
+
+UPDATE KOCURY
+SET PRZYDZIAL_MYSZY = 80
+WHERE IMIE = 'JACEK';
+
+SELECT * FROM Kocury;
+SELECT * FROM Proby_wykroczenia;
+
+ROLLBACK;
+
+DROP TABLE Proby_wykroczenia;
+DROP TRIGGER trg_monitor_wykroczenia;
