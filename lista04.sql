234992eaf5c93fa0294ecfe6ca53f037ab9d5d7b
Filipstrozik <filipshelby@gmail.com>
2023-01-05 17:55:51 +0100

    started lista4

--- lista04.sql
+++ lista04.sql
@@ -1,22 +1,32 @@
 --zad47
-CREATE OR REPLACE TYPE KocuryO AS OBJECT (
-    imie VARCHAR2(15),
-    plec VARCHAR2(1),
-    pseudo VARCHAR2(15),
-    funkcja VARCHAR2(10),
-    szef VARCHAR2(15),
-    w_stadku_od DATE,
+CREATE OR REPLACE TYPE KocuryO AS OBJECT
+(
+    imie            VARCHAR2(15),
+    plec            VARCHAR2(1),
+    pseudo          VARCHAR2(15),
+    funkcja         VARCHAR2(10),
+    szef            REF KocuryO,
+    w_stadku_od     DATE,
     przydzial_myszy NUMBER(3),
-    myszy_extra NUMBER(3),
-    nr_bandy NUMBER(2),
-    MEMBER FUNCTION caly_przydzial RETURN NUMBER);
+    myszy_extra     NUMBER(3),
+    nr_bandy        NUMBER(2),
+    MEMBER FUNCTION caly_przydzial RETURN NUMBER,
+    MAP MEMBER FUNCTION info RETURN VARCHAR2
+);
+
+DROP TYPE KocuryO;
 
 ROLLBACK;
 
 CREATE OR REPLACE TYPE BODY KocuryO
-    AS MEMBER FUNCTION caly_przydzial RETURN NUMBER IS
+AS
+    MEMBER FUNCTION caly_przydzial RETURN NUMBER IS
     BEGIN
-        RETURN NVL(przydzial_myszy,0) + NVL(myszy_extra,0);
+        RETURN NVL(przydzial_myszy, 0) + NVL(myszy_extra, 0);
+    END;
+    MAP MEMBER FUNCTION info RETURN VARCHAR2 IS
+    BEGIN
+        RETURN imie || ', ' || plec ||', pseudo:' || pseudo || ' funkcja: '|| funkcja ||', zjada:'|| SELF.CALY_PRZYDZIAL();
     END;
 END;
 
@@ -24,22 +34,28 @@ END;
 CREATE TABLE KocuryT OF KocuryO (
   imie CONSTRAINT kocuryo_imie_nn NOT NULL,
   plec CONSTRAINT kocuryo_plec_ch CHECK(plec IN ('M', 'D')),
-  pseudo CONSTRAINT kocuryo_pseudo_pk PRIMARY KEY (pseudo),
+  pseudo CONSTRAINT kocuryo_pseudo_pk PRIMARY KEY,
   funkcja CONSTRAINT ko_f_fk REFERENCES Funkcje(funkcja),
-  szef SCOPE IS KocuryO,
+  szef SCOPE IS KocuryT,--to nie działa
   w_stadku_od DEFAULT SYSDATE,
-  CONSTRAINT ko_nr_fk REFERENCES Bandy(nr_bandy)
+  nr_bandy CONSTRAINT ko_nr_fk REFERENCES Bandy(nr_bandy)
 );
 
+DROP TABLE KocuryT;
+
 --plebs
-CREATE OR REPLACE TYPE PlebsO AS OBJECT (
-    kot REF KocuryO,
-    MEMBER FUNCTION get_details RETURN VARCHAR2);
+CREATE OR REPLACE TYPE PlebsO AS OBJECT
+(
+    pseudo   VARCHAR2(15),
+    kot       REF KocuryO,
+    MEMBER FUNCTION get_details RETURN VARCHAR2
+);
 
 
 CREATE OR REPLACE TYPE BODY PlebsO
-    AS MEMBER FUNCTION get_details RETURN VARCHAR2
-    IS
+AS
+    MEMBER FUNCTION get_details RETURN VARCHAR2
+        IS
         details VARCHAR2(400);
     BEGIN
         SELECT 'IMIE: ' || DEREF(kot).imie || ' PSEUDO ' || DEREF(kot).pseudo INTO details FROM dual;
@@ -47,15 +63,20 @@ CREATE OR REPLACE TYPE BODY PlebsO
     END;
 END;
 
+CREATE TABLE PlebsT OF PlebsO(
+    kot SCOPE IS KocuryT CONSTRAINT plebso_kot_nn NOT NULL,
+    CONSTRAINT plebso_fk FOREIGN KEY (pseudo) REFERENCES KocuryT(pseudo),
+    CONSTRAINT plebso_pk PRIMARY KEY (pseudo));
 
 
 --elita
-CREATE OR REPLACE TYPE ElitaO AS OBJECT (
-    id INTEGER,
-    kocur REF KocuryO,
-    slugus REF PlebsO,
+CREATE OR REPLACE TYPE ElitaO AS OBJECT
+(
+    pseudo VARCHAR2(15),
+    kot      REF KocuryO,
+    slugus   REF PlebsO,
     MEMBER FUNCTION get_sluga RETURN REF PlebsO
-                                        );
+);
 
 CREATE OR REPLACE TYPE BODY ElitaO AS
   MEMBER FUNCTION get_sluga RETURN REF PlebsO IS
@@ -64,6 +85,44 @@ CREATE OR REPLACE TYPE BODY ElitaO AS
     END;
 END;
 
+
+CREATE TABLE ElitaT OF ElitaO(
+    pseudo CONSTRAINT elitao_pseudo_pk PRIMARY KEY,
+    kot SCOPE IS KocuryT CONSTRAINT elitao_kot_nn NOT NULL,
+    slugus SCOPE IS PlebsT
+);
+
+CREATE OR REPLACE TYPE KontoO AS OBJECT
+(
+    nr_myszy NUMBER(5),
+    data_wprowadzenia DATE,
+    data_usuniecia DATE,
+    kot REF ElitaO,
+    MEMBER PROCEDURE wyprowadz_mysz(dat DATE),
+    MAP MEMBER FUNCTION GET_INFO RETURN VARCHAR2
+);
+
+CREATE OR REPLACE TYPE BODY KontoO AS
+MAP MEMBER FUNCTION GET_INFO RETURN VARCHAR2 IS
+    wl ElitaO;
+    kot KocuryO;
+    BEGIN
+        SELECT DEREF(kot) INTO wl FROM DUAL;
+        SELECT DEREF(wl.kot) INTO kot FROM DUAL;
+        RETURN TO_CHAR(data_wprowadzenia) || ' ' || kot.PSEUDO || TO_CHAR(data_usuniecia);
+    END;
+    MEMBER PROCEDURE wyprowadz_mysz(dat DATE) IS
+    BEGIN
+      data_usuniecia := dat;
+    END;
+END;
+-- not implemented
+CREATE TABLE KontoT OF KontoO (
+    nr_myszy CONSTRAINT kontaO_n_pk PRIMARY KEY,
+    wlasciciel SCOPE IS ElitaT CONSTRAINT ko_w_nn NOT NULL,
+    data_wprowadzenia CONSTRAINT ko_dw_nn NOT NULL,
+    CONSTRAINT ko_dw_du_ch CHECK(data_wprowadzenia <= data_usuniecia)
+);
 --incydenty
 
 --dane wprowadzenia myszy
